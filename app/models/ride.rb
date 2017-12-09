dcfd5a6e4c866f079a6f067b21b8b3976f98adbd
github username <github email address>
2017-12-08 22:20:16 -0500

    finished models/migrations

--- app/models/ride.rb
+++ app/models/ride.rb
@@ -1,3 +1,26 @@
 class Ride < ActiveRecord::Base
-  # write associations here
+  belongs_to :user
+  belongs_to :attraction
+
+  def take_ride
+    user = User.find_by(id: self.user_id)
+    attraction = Attraction.find_by(id: self.attraction_id)
+    msg_height = "You are not tall enough to ride the #{attraction.name}."
+    msg_tickets = "You do not have enough tickets to ride the #{attraction.name}."
+
+    if user.tickets < attraction.tickets and user.height < attraction.min_height
+      "Sorry. #{msg_tickets} #{msg_height}"
+    elsif user.tickets < attraction.tickets
+      "Sorry. #{msg_tickets}"
+    elsif user.height < attraction.min_height
+      "Sorry. #{msg_height}"
+    else
+      # binding.pry
+      user.tickets -= attraction.tickets
+      user.happiness += attraction.happiness_rating
+      user.nausea += attraction.nausea_rating
+      user.save
+    end
+  end
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,17 @@
 class User < ActiveRecord::Base
-  # write associations here
+  has_many :rides
+  has_many :attractions, through: :rides
+  validates_presence_of :name
+  has_secure_password
+
+  def mood
+    if self.happiness > self.nausea
+      "happy"
+    elsif self.happiness < self.nausea
+      "sad"
+    else
+      "neutral"
+    end
+  end
+
 end
